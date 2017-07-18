3b6745f3a7245e4ac2cf4154344f37b1c2de244c
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2017-07-18 15:06:49 +0200

    update: as.integer

--- movie-analysis.R
+++ movie-analysis.R
@@ -63,25 +63,57 @@ for(i in 1:nrow(mm))
   mm[i,which(colnames(mm) %in% movgen2[i,])] <- 1
 }
 
+#' dummy change of name...
 movgen_matrix2 <- mm
+
+#' convert to int, checking if that works
+movgen_matrix2 <- as.data.frame(movgen_matrix2[-1,], stringsAsFactors=FALSE) #remove first row, which was the genre list
+for (c in 1:ncol(movgen_matrix2)) {
+  movgen_matrix2[,c] <- as.integer(movgen_matrix2[,c])
+} #convert from characters to integers
+
 
 #' matrix of rating users
 #' 0 para null values, 1 for ratings over 3, -1 for ratings less or equal than 3
 #'  
-binary_ratings <- ratings
-binary_ratings[which(binary_ratings$rating <=3),"rating"] <- -1
-binary_ratings[is.na(binary_ratings$rating),"rating"] <- 0
-binary_ratings[which(binary_ratings$rating > 3),"rating"] <- 1
+binary_ratings <- as.data.frame(ratings, stringsAsFactors = FALSE)
+binary_ratings[which(binary_ratings$rating <=3),"rating"] <- as.integer(-1)
+binary_ratings[is.na(binary_ratings$rating),"rating"] <- as.integer(0)
+binary_ratings[which(binary_ratings$rating > 3),"rating"] <- as.integer(1)
 
 #' is there any null value yet?
 binary_ratings[!complete.cases(binary_ratings),]
 
-# professor's code
+#' professor's code
+#' por qué queda una matrix de 9066 x 671??
+#' 9066 revisiones o reviews
+#' Rows are movieIds, cols are userIds
+#' profesor's code:
 binary_ratings2 <- dcast(binary_ratings, movieId~userId, value.var = "rating", na.rm=FALSE)
 for (i in 1:ncol(binary_ratings2)){
-  binary_ratings2[which(is.na(binary_ratings2[,i]) == TRUE),i] <- 0
+  binary_ratings2[which(is.na(binary_ratings2[,i]) == TRUE),i] <- as.integer(0)
 }
 
 binary_ratings2 = binary_ratings2[,-1] #remove movieIds col. Rows are movieIds, cols are userIds
 
 dim(binary_ratings2)
+
+#' You might notice that the movies dataset has 9125 movies, but the ratings dataset only has 9066
+#' movies. To deal with this, remove the movies that have never been rated from the genres matrix.
+#' Use the code below:
+unique_movieIds <- unique(movies$movieId) #9125
+unique_ratings <- unique(ratings$movieId) #9066
+movies2 <- movies[-which((unique_movieIds %in% unique_ratings) == FALSE),]
+rownames(movies2) <- NULL
+#Remove rows that are not rated from movgen_matrix2
+movgen_matrix3 <- movgen_matrix2[-which((unique_movieIds %in% unique_ratings) == FALSE),]
+rownames(movgen_matrix3) <- NULL
+#
+
+#' checking dimensions
+dim(movgen_matrix3)
+dim(binary_ratings2)
+# Now we can calculate the dot product of the genre matrix (movgen_matrix3) and the ratings
+# matrix (binary_ratings2) and obtain the user profiles. Ensure you convert to binary scale
+
+user_profile <- t(movgen_matrix3)*binary_ratings2
